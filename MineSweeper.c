f9369084f701e2d0bfa349c0e236c9157e503ae7
Hamed <54506231+Hawmex@users.noreply.github.com>
2021-05-11 17:36:58 +0430

    Create MineSweeper.c

---
+++ MineSweeper.c
@@ -0,0 +1,54 @@
+#include "stdio.h"
+
+int main()
+{
+    size_t rows;
+    size_t columns;
+
+    scanf("%zu%zu", &rows, &columns);
+
+    unsigned int counter = 0;
+
+    while (rows != 0 || columns != 0)
+    {
+        counter += 1;
+
+        int field[rows][columns];
+
+        for (size_t r = 0; r < rows; r++)
+        {
+            scanf("\n");
+
+            for (size_t c = 0; c < columns; c++)
+                field[r][c] = getchar();
+        }
+
+        printf("Field #%u:\n", counter);
+
+        for (size_t r = 0; r < rows; r++)
+        {
+            for (size_t c = 0; c < columns; c++)
+            {
+                if (field[r][c] == 42)
+                    printf("%s", "*");
+                else
+                {
+                    unsigned int adjacentMines = 0;
+
+                    for (size_t adjacentRow = r == 0 ? r : r - 1; adjacentRow <= (r == rows - 1 ? r : r + 1); adjacentRow++)
+                        for (size_t adjacentColumn = c == 0 ? c : c - 1; adjacentColumn <= (c == columns - 1 ? c : c + 1); adjacentColumn++)
+                            if (field[adjacentRow][adjacentColumn] == 42)
+                                adjacentMines += 1;
+
+                    printf("%u", adjacentMines);
+                }
+            }
+
+            puts("");
+        }
+
+        scanf("%zu%zu", &rows, &columns);
+    }
+
+    return 0;
+}
